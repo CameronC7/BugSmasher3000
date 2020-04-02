9438b6d067b19f2cefb0f1cceddcb47e0df6d0e1
Cameron <cameroncox007@hotmail.com>
2020-04-02 08:18:41 -0700

    Add project files.

---
+++ BugSmasher3000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BugSmasher3000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
